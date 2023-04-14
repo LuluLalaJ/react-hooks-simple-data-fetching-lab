a462ed245cc1df9b9a493b5dbf98bc287ef7607d
Chen Jiang <chen.jiang.1206@gmail.com>
2023-04-14 09:20:39 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, { useEffect, useState } from 'react'
+
+function App() {
+    const apiUrl = "https://dog.ceo/api/breeds/image/random"
+
+    const [dogImage, setDogImage] = useState('')
+
+    useEffect(() => {
+        fetch(apiUrl)
+        .then(r => r.json())
+        .then(data => setDogImage(data.message))
+    }, [])
+
+    if (!dogImage) {
+        return <p>Loading...</p>
+    }
+    return (
+        <img
+            alt="A Random Dog"
+            src={dogImage}
+        ></img>
+    )
+}
+
+
+export default App 
